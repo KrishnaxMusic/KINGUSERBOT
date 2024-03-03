a722605391736526a85ba4513e56aeacbda58958
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-03-03 12:08:08 +0530

    Update help.py

--- Branded/plugins/tools/help.py
+++ Branded/plugins/tools/help.py
@@ -57,12 +57,12 @@ async def help_button(client, query):
     back_match = re.match(r"help_back", query.data)
     top_text = f"""
 **🥀 Welcome To Help Menu Of
-Daxx Userbot » {__version__} ✨...
+Brabded Userbot » {__version__} ✨...
 
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [DAXX Server](https://t.me/DAXXSUPPORT).**
+🌷Powered By : [DAXX Server](https://t.me/BRANDED_WORLD).**
 """
     
     if plug_match:
